97c619ac50d23bd1468af9aea452cd3c4faa30d4
IT-Egor <egor.belousov.04@yandex.ru>
2025-06-10 20:29:21 +0300

    add server

---
+++ main-service/src/main/java/ru/itegor/antiplagiacode/AntiplagiacodeServer.java
@@ -0,0 +1,11 @@
+package ru.itegor.antiplagiacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiplagiacodeServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AntiplagiacodeServer.class, args);
+    }
+}
